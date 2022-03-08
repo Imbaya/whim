29defc97a094ae3df52699d60d8f86c776972149
Jerry Valentine <valentineoloo@gmail.com>
2022-03-08 17:25:10 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+
+
+import React from 'react'
+import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+import HomeScreen from './src/screen/HomeScreen'
+
+
+const App = () => {
+  return (
+    <View style = {styles.container} > 
+      <HomeScreen />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1
+  }
+
+})
